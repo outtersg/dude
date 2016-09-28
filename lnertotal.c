b60f721e40bf063e87313a30644b9328893257e1
Guillaume Outters <guillaume-scripts@outters.eu>
2016-09-28 06:37:42 +0200

    lnertotal: paramètres
    + Gestion des paramètres "racine".
    + Gestion du -r (reverse, ou reference).
    
    darcs-hash:3afb477c00bd967d1178ba4604fc760ce8f8d60e

--- lnertotal.c
+++ lnertotal.c
@@ -6,4 +6,22 @@
 
 int main(int argc, char ** argv)
 {
+	/* On pousse par défaut en sens inverse. Le dernier argument est considéré comme la référence (à analyser en premier, donc), afin que sur du 2016-*, le dernier en date soit considéré comme la référence. */
+	
+	int sens = -1;
+	int i;
+	int nFichiers;
+	char ** fichiers = (char **)malloc(argc * sizeof(char *)); /* Mieux vaut réserver trop que pas assez. */
+	
+	for(i = 0; ++i < argc;)
+		if(0 == strcmp(argv[i], "-r"))
+			sens = -sens;
+		else if(sens > 0)
+			fichiers[nFichiers++] = argv[i];
+		else
+		{
+			memmove(&fichiers[1], fichiers, (argc - 1) * sizeof(char *));
+			fichiers[0] = argv[i];
+			++nFichiers;
+		}
 }
