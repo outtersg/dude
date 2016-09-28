42256991b923f1c4857f39932e0d83060fd5daf7
Guillaume Outters <guillaume-scripts@outters.eu>
2016-09-28 06:34:54 +0200

    lnertotal: lner sans critère sur le nom de fichier
    + Fichier vide (pour le moment).
    = Il est prévu que lnertotal se cale uniquement sur le md5 des fichiers pour déterminer qu'ils sont identiques. Le but est de pouvoir détecter les doublons même lorsqu'ils s'appellent IMG_7621.JPG d'un côté et Pauline_fait_du_vélo.JPG de l'autre.
    
    darcs-hash:8c1185b94d1a0c219f24aeca5f05972b55ebb1ef

---
+++ lnertotal.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Cet utilitaire est destiné à tourner sur un Mac OS X 10.8; les optimisations du style linkat, fdopendir, sont donc remises à plus tard. */
+
+int main(int argc, char ** argv)
+{
+}
